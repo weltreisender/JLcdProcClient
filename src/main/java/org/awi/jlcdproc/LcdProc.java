7200395a394482c83086578befbd06b8014222fb
weltreisender <weltreisender_aw@yahoo.de>
2014-12-30 15:38:47 +0100

    menu package added

--- src/main/java/org/awi/jlcdproc/LcdProc.java
+++ src/main/java/org/awi/jlcdproc/LcdProc.java
@@ -1,11 +1,17 @@
 package org.awi.jlcdproc;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import org.awi.jlcdproc.events.Event;
+import org.awi.jlcdproc.events.EventListener;
+import org.awi.jlcdproc.events.SuccessEvent;
 import org.awi.jlcdproc.io.Connection;
+import org.awi.jlcdproc.menu.Action;
+import org.awi.jlcdproc.menu.MainMenu;
+import org.awi.jlcdproc.menu.Menu;
 import org.awi.jlcdproc.widgets.Direction;
 import org.awi.jlcdproc.widgets.Heartbeat;
-import org.awi.jlcdproc.widgets.Icon;
 import org.awi.jlcdproc.widgets.ScrollerWidget;
 
 public class LcdProc implements AutoCloseable {
@@ -14,40 +20,110 @@ public class LcdProc implements AutoCloseable {
 
 	private int currentScreenId = 0;
 
-	public void init() throws Exception {
+	public LcdProc() throws Exception {
 
-		init("localhost", 13666);
+		this("localhost", 13666);
 	}
 
-	public void init(String host, int port) throws Exception {
+	public LcdProc(String host, int port) throws Exception {
 
 		connection = new Connection(host, port);
 		connection.connect();
 	}
 
-	public Screen createScreen() throws Exception {
+	public void clientName(String name) throws Exception {
+
+		connection.send("client_set", "-name", name);
+	}
+
+	public Screen screen() throws Exception {
 
 		return new Screen(connection, currentScreenId++);
 	}
 
-	public Screen createScreen(String screenId) throws Exception {
+	public Screen screen(String screenId) throws Exception {
 
 		return new Screen(connection, screenId);
+	}
+
+	public MainMenu mainMenu(String name) {
+
+		return new MainMenu(connection, name);
+	}
+
+	public void addEventListener(EventListener eventListener) {
+
+		connection.addEventListener(eventListener);
+	}
+
+	public void removeEventListener(EventListener eventListener) {
+
+		connection.removeEventListener(eventListener);
 	}
 
 	public static void main(String[] args) throws Exception {
 
 		try (LcdProc lcdProc = new LcdProc()) {
 
-			lcdProc.init();
-
-			Screen screen = lcdProc.createScreen("myScreen");
-
-			screen.setHeartbeat(Heartbeat.OFF);
+			Screen s = lcdProc.screen();
+			s.setHeartbeat(Heartbeat.OFF);
 			
-			ScrollerWidget w = screen.scrollerWidget("myWidget");
-			w.set(1, 1, 16, 2, Direction.MARQUEE, 3, "Dies ist ein sehr langer Text, bei dem Scrollen notwendig ist ");
-		
+			MainMenu mainMenu = lcdProc.mainMenu("Main menu");
+			
+			lcdProc.addEventListener((Event event) -> { if (!(event instanceof SuccessEvent)) {System.out.println(event); } });
+
+			Menu menu1 = mainMenu.addMenu("m1", "Menu 1");
+			Action a11 = menu1.addAction("a11", "Action 1.1");
+			Action a12 = menu1.addAction("a12", "Action 1.2");
+			Action a13 = menu1.addAction("a13", "Action 1.3");
+
+			a11.next(a12);
+			a12.prev(a11).next(a13);
+			a13.prev(a12);
+			
+			Menu menu2 = mainMenu.addMenu("m2", "Menu 2");
+			menu2.addAction("a21", "Action 2.1");
+			menu2.addAction("a22", "Action 2.2");
+			menu2.addAction("a23", "Action 2.3");
+
+			
+//		
+//			for (int b = 0; b < 32; b++) {
+//
+//				StringBuilder sb = new StringBuilder();
+//				sb.append(b).append(": <");
+//				switch(b) {
+//				
+//				case 0:
+//					sb.append("nul");
+//					break;
+//				case 10:
+//					sb.append("lf");
+//					break;
+//				case 13:
+//					sb.append("cr");
+//					break;
+//				case 34:
+//					sb.append("\\\"");
+//					break;
+//				case 92:
+//					sb.append("\\\\");
+//					break;
+//				default:	
+//					sb.append(new String(new byte[] {(byte)(b & (byte)0xFF)}));
+//					break;
+//				}
+//				
+//				sb.append("> ");
+//				
+//				mainMenu.addAction(String.format("a%d", b), sb.toString());
+//				
+//			}
+			
+			// ScrollerWidget widget = s.scrollerWidget();
+			// widget.set(1, 1, 16, 1, Direction.MARQUEE, 1, sb.toString());
+			mainMenu.activate();
+			
 			System.in.read();
 		}
 	}
--- src/main/java/org/awi/jlcdproc/io/CommandCompletedListener.java
+++ src/main/java/org/awi/jlcdproc/io/CommandCompletedListener.java
@@ -6,22 +6,30 @@ import java.util.concurrent.atomic.AtomicReference;
 import org.awi.jlcdproc.events.Event;
 import org.awi.jlcdproc.events.EventListener;
 
-public class CommandCompletedListener<T extends Event> extends EventListener<T> implements Callable<T> {
+public class CommandCompletedListener implements EventListener, Callable<Event> {
 
-	private AtomicReference<T> event = new AtomicReference<>();
+	private AtomicReference<Event> event = new AtomicReference<>();
 	
-	public CommandCompletedListener(Class<? extends T> expectedClass) {
-		super(expectedClass);
+	private final Class<? extends Event> expectedEvent;
+	
+	public CommandCompletedListener(Class<? extends Event> expectedEvent) {
+
+		this.expectedEvent = expectedEvent;
 	}
 	
 	@Override
-	public void onEvent(T event) {
+	public void onEvent(Event event) {
+		
+		if (!expectedEvent.isInstance(event)) {
+			
+			return;
+		}
 		
 		this.event.set(event);
 	}
 
 	@Override
-	public T call() throws Exception {
+	public Event call() throws Exception {
 
 		while (event.get() == null){
 			Thread.sleep(50);
